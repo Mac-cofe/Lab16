25c7dc2c2363bc55917243f6028885c70eab563d
M.M.Karmanova@urfu.me <M.M.Karmanova@urfu.me>
2021-10-21 14:00:51 +0500

    Изменения кода

--- Lab16_Exp2/Program.cs
+++ Lab16_Exp2/Program.cs
@@ -8,7 +8,7 @@ using System.Text.Json;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 using System.Text.Json.Serialization;
-using Newtonsoft.Json;
+//using Newtonsoft.Json;
 
 namespace Lab16_Exp2
 {
@@ -18,13 +18,9 @@ namespace Lab16_Exp2
         {
 
             string path = @"C:\Exp2\Products.json";
-            //string[] products;
             int i = 0;
             int k = 0;
-            string product_s = "";
-            //string line;
-            //double maxPrice = 0;
-            //string nameProduct_maxPrice = "";
+            string product_s;
             JsonSerializerOptions options = new JsonSerializerOptions()
             {
                 Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
@@ -32,42 +28,23 @@ namespace Lab16_Exp2
             };
 
             Console.WriteLine("В файле хранятся данные о следующих товарах:");
+            product_s = File.ReadAllText(path);
+            Console.WriteLine(product_s);
 
-            using (FileStream fstream = File.OpenRead(path))
-            {
-                // преобразуем строку в байты
-                byte[] array = new byte[fstream.Length];
-                // считываем данные
-                fstream.Read(array, 0, array.Length);
-                // декодируем байты в строку
-                product_s = Encoding.UTF8.GetString(array);
-                //Console.WriteLine($"Текст из файла: {product_s}");
-            }
+            Product product1 = JsonSerializer.Deserialize<Product>(product_s);  // ошибка
 
-            //Console.WriteLine(product_s);
-
-            Product product6 = System.Text.Json.JsonSerializer.Deserialize<Product>(product_s);
-            
-            
-
-
-
-
-
-
-
-
-            //Console.WriteLine($"Самый дорогой товар {nameProduct_maxPrice}, его стоимость {maxPrice} за 1 кг.");
             Console.WriteLine("Текст прочитан");
             Console.ReadKey();
         }
     }
 
-    class Product
+
+    public class Product
     {
         public int KodProduct { get; set; }
         public string NameProduct { get; set; }
         public double PriceProduct { get; set; }
 
     }
+
 }
